4497dbc125cf18d22ef295fc54227615ab18592a
Franco Valdez <franco.martin.valdez@mi.unc.edu.ar>
2023-02-17 19:14:56 -0300

    ultimo cmmit

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
     detail: [],
   });
   useEffect(() => {
-    fetch("https://restcountries.com/v3.1/all/")
+    fetch("https://restcountries.com/v3.1/all")
       .then((response) => response.json())
       .then((data) =>
         setStates({
